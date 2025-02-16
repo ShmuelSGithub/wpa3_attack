9c477ebd5a5b2001bd7b784c9e6f1c21faeb76bd
ShmuelSGithub <100469936+ShmuelSGithub@users.noreply.github.com>
2025-02-16 21:46:40 +0200

    Update datareader.py

--- .idea/datareader.py
+++ .idea/datareader.py
@@ -15,3 +15,15 @@ def qplot(df):
         tmp=plt.plot(range(50),[df[1][df[0]==j].quantile(i/50) for i in range(50)],label=j)
     #plt.legend() if you care about which address matches which graph
     #plt.show() if you don't wanna add anything else
+
+#the default case(password abcdefgh, 20 addresses) is 2, 1, 2, 1, 1, 1, 1, 2, 3, 1, 2, 3, 1, 1, 1, 1, 9, 1, 1, 2
+def min_iter(df,addrs=20):
+    '''retuns a DataFrame list of minimum possible iterations for each address, reliebly identifies as long as there are no gaps with 0 addresses'''
+    q=pd.DataFrame([(j,t[1][t[0]==j].quantile(0.15),t[1][t[0]==j].quantile(0.35)) for j in range(addrs)])
+    q[3]=0
+    i=1
+    while(not q[3].all() and i<256):
+        idmin=q[1][q[3]==0].idxmin()#get the index with smallest quantile to not be selected
+        q.loc[np.logical_and(q[3]==0,q[1]<=q[2][idmin]),3]=i#assign min iterations to all intersecting quantiles
+        i+=1
+    return q[3]
