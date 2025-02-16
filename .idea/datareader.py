1fcd26cc108b8301edbc8cc30ea4d223e4e963c8
ShmuelSGithub <100469936+ShmuelSGithub@users.noreply.github.com>
2025-02-16 18:44:13 +0200

    Update datareader.py
    added the graph plotter

--- .idea/datareader.py
+++ .idea/datareader.py
@@ -9,3 +9,8 @@ def data(path):
     T=[str.split(a," ") for a in T]
     T=[(int(a[1][:-1],16),int(a[2][:-1],10)) for a in T]
     return pd.DataFrame(T)
+
+def qplot(df):
+    for j in range(20):
+        tmp=plt.plot(range(50),[df[1][df[0]==j].quantile(i/50) for i in range(50)])
+        #plt.show() if you don't wanna add anything else
